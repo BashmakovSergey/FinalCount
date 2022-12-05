f192aa3aa5371584cb1241c558eb1f18460b34bd
Bashmakov Sergey <bashmakovsergey@yandex.ru>
2022-12-05 15:35:14 +0300

    1

--- FinalCount/ViewController.swift
+++ FinalCount/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     var counter: Int = 0
+
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var counterButton: UIButton!
+    @IBAction func title(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счетчика: \(counter)"
+    }
+    
 }
 
